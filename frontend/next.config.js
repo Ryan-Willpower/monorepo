6126520d3e0501303607bc6d7cccac744a924e73
Surasak Chaisurin <surasak.c.work@gmail.com>
2021-10-30 22:13:45 +0700

    feat: resolve dependency with ts-loader

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,27 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, context) => {
+    console.log(config.module.rules);
+
+    config.module.rules.push({
+      test: /.(ts|tsx)$/,
+      include: [path.resolve(__dirname, "../shared")],
+      loader: "ts-loader",
+    });
+
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        shared: path.resolve(__dirname, "../shared"),
+      },
+    };
+
+    // console.log(config);
+
+    return config;
+  },
+};
